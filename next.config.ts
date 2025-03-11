2086e6037336165d798b31acaf03ee9013e7ec0d
prasunet <162030269+prasunet@users.noreply.github.com>
2025-03-12 01:19:27 +0530

    Update next.config.ts
    Signed-off-by: prasunet <162030269+prasunet@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack(config) {
+    // This rule allows you to import JSON files
+    config.module.rules.push({
+      test: /\.json$/,
+      loader: 'json-loader',
+      type: 'javascript/auto', // Make sure JSON is treated as JavaScript module
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
