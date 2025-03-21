f44d1805d7b402f22de942bd9e91e266f24afc09
prasunet <162030269+prasunet@users.noreply.github.com>
2025-03-22 00:27:37 +0530

    Update page.tsx
    Signed-off-by: prasunet <162030269+prasunet@users.noreply.github.com>

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,15 @@
 import ContactForm from "@/components/home/Cont";
+import Navbar from "@/components/home/header/navbar";
 
 export default function ContactPage() {
   return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
-      <ContactForm />
-    </div>
+    <>
+      <Navbar />
+      <div className="container mx-auto p-8">
+        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+        <ContactForm />
+      </div>
+    </>
   );
 }
+
