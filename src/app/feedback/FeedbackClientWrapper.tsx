41b0915091ab03c2ed47319edfbc31f317ea08da
Prasunet <162030269+z9xqv7mkt3wl8psa@users.noreply.github.com>
2025-04-29 00:54:37 +0530

    Create FeedbackClientWrapper.tsx
    Signed-off-by: Prasunet <162030269+z9xqv7mkt3wl8psa@users.noreply.github.com>

---
+++ src/app/feedback/FeedbackClientWrapper.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const FeedbackClient = dynamic(() => import('./FeedbackClient'), {
+    ssr: false,
+});
+
+export default function FeedbackClientWrapper() {
+    return <FeedbackClient />;
+}
